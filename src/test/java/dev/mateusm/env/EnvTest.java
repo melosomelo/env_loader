3080279d00110c906e24b992c0749b84ba83eca9
melosomelo <mateusmelornrn@gmail.com>
2023-05-21 11:28:42 -0300

    remoev some unused imports

--- src/test/java/dev/mateusm/env/EnvTest.java
+++ src/test/java/dev/mateusm/env/EnvTest.java
@@ -1,16 +1,8 @@
 package dev.mateusm.env;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThrows;
-import static org.junit.Assert.assertTrue;
-
-import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.IOException;
-
 import org.junit.Test;
-
-import dev.mateusm.env.exceptions.BadEnvFileException;
 
 public class EnvTest {
 
