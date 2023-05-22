247a1552d81b8d6439b1d6ab12e56401546de1c7
melosomelo <mateusmelornrn@gmail.com>
2023-05-22 16:02:47 -0300

    cleanup EnvFileCreator

--- src/test/java/dev/mateusm/env/EnvFileCreator.java
+++ src/test/java/dev/mateusm/env/EnvFileCreator.java
@@ -3,23 +3,8 @@ package dev.mateusm.env;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.function.Consumer;
 
 public class EnvFileCreator {
-  public static void makeFromValues(String filename, String... keysAndValues) {
-    StringBuilder strBuilder = new StringBuilder();
-    System.out.println(keysAndValues.length);
-  }
-
-  public static File makeFromContent(String filename, String content) throws IOException {
-    File file = new File(filename);
-    file.createNewFile();
-    FileWriter writer = new FileWriter(file);
-    writer.write(content);
-    writer.close();
-    return file;
-  }
-
   // Utility method to encapsulate the logic of creating and deleting temporary
   // files to
   // test parsers. It accepts a runnable that will be the actions the test can
